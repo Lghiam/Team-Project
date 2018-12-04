02499fa5e5baf7f09bea618e23aebb6c5a2319e6
jack97 <jackgallaher36@gmail.com>
2018-12-04 12:32:32 +0000

    Map
    New Api key, error when running on latop but works on phone

--- Android Studio/Project_2018/app/src/main/java/com/project/moverskeletalapp/ReviewActivity.java
+++ Android Studio/Project_2018/app/src/main/java/com/project/moverskeletalapp/ReviewActivity.java
@@ -1,29 +1,73 @@
 package com.project.moverskeletalapp;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ReviewActivity extends AppCompatActivity {
+
+    private RatingBar ratingBar;
+    private String id;
+    private String ratingBarDisplay;
+    private String Comments;
+
+    DatabaseReference databaseReference;
+    ProgressDialog progressDialog;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        final EditText Comments = (EditText) findViewById(R.id.CommentET);
+        final RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        Button SubmitBtn = (Button) findViewById(R.id.SubmitBtn);
+        final TextView ratingBarDisplay = (TextView) findViewById(R.id.DisplayET);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+        id = firebaseAuth.getCurrentUser().getUid();
+
+        SubmitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                ratingBarDisplay.setText("your Rating is " + ratingBar.getRating());
+                backpage();
             }
         });
     }
 
-}
+
+    public void backpage(){
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("RatingsBase").child(id);
+
+        databaseReference.child("UID").setValue(id);
+        databaseReference.child("Ratings").setValue(ratingBarDisplay);
+        databaseReference.child("comments").setValue(Comments);
+
+
+        progressDialog.setMessage("Thanks for you rating us");
+        progressDialog.show();
+
+        Intent intent = new Intent (this, MoverDisplayedDetails.class);
+        startActivity(intent);
+    }
+
+    }
+
+
